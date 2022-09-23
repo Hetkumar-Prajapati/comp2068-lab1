e3e12b8748ac9e5923ce26af8fc1e6bbf50dca5a
hetkumar1704 <200476049@student.gerogianc.on.ca>
2022-09-23 17:46:56 -0400

    my final commit

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const app = express()
 //add http request handler
 let mom = (req,res)=>{
     res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-    res.write('hello world')
+    res.write('My mom is a lovely lady. I like her art of cooking and she is an extra-ordinary HomeMakker. She a very great conunciler and obviously i love her the most.')
     res.end()
 }
 
 let dad = (req,res)=>{
     res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-    res.write('goodbye world')
+    res.write('My dad is a great bussiness man. I get shock to see his ideas to get customers attracted to the bussiness and make profit out of it. He is a great helper to people and is well know for is good doing in the locality.')
     res.end()
 }
 
 let me = (req,res)=>{
     res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-    res.write('goodbye world')
+    res.write('I really love doing html,css, and js. Further more i am learning ReactJs , basically use of props and useState in react and reactdom. I am so excited to start some job in front end but unfortunatelly i am unable to get co-op. Fingers crossed for this winter co-op!!')
     res.end()
 }
 
 let Home = (req,res)=>{
     if (req.url === '/'){
     res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-    res.write('Home Page')
+    res.write('Home Page!!')
     res.end()
     }
     else{
